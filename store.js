667681da554174110b05abca7e6d27c847edcf5c
saramazal <32996161+saramazal@users.noreply.github.com>
2021-08-04 11:31:02 +0300

    Update store.js

--- store.js
+++ store.js
@@ -1,8 +1,43 @@
+cimport allRecipesData from './data.js';
+
 const initialState = {
-   allRecipes: [],
+  allRecipes: [],
   favoriteRecipes: [],
   searchTerm: ''
 };
-const todosReducer = (state = initialState, action) => {
-  // rest of todosReducer logic omitted
-};
+
+// Dispatched when the user types in the search input.
+// Sends the search term to the store.
+const setSearchTerm = (term) => {
+  return { 
+    type: 'searchTerm/setSearchTerm', 
+    payload: term 
+  };
+}
+
+// Dispatched when the user presses the clear search button.
+const clearSearchTerm = () => {
+  return { 
+    type: 'searchTerm/clearSearchTerm' 
+  };
+}
+
+// Dispatched when the user first opens the application.
+// Sends the allRecipesData array to the store.
+const loadData = () => {
+
+}
+
+// Dispatched when the user clicks on the heart icon of 
+// a recipe in the "All Recipes" section.
+// Sends the recipe object to the store.
+const addRecipe = (recipe) => {
+
+}
+
+// Dispatched when the user clicks on the broken heart 
+// icon of a recipe in the "Favorite Recipes" section.
+// Sends the recipe object to the store.
+const removeRecipe = (recipe) => {
+
+}
