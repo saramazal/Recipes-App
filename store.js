9eb0598e88227a07851cc75d54e167d037dacef0
saramazal <32996161+saramazal@users.noreply.github.com>
2021-08-04 11:26:33 +0300

    Create store.js

---
+++ store.js
@@ -0,0 +1,8 @@
+const initialState = {
+   allRecipes: [],
+  favoriteRecipes: [],
+  searchTerm: ''
+};
+const todosReducer = (state = initialState, action) => {
+  // rest of todosReducer logic omitted
+};
